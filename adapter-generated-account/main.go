7f16028c6460df3f49dc94ad580d4464b7839770
nata6ify@gmail.com <nata6ify@gmail.com>
2024-12-05 21:41:58 +0700

    [adapter-generate-account] change to echo v2

--- adapter-generated-account/main.go
+++ adapter-generated-account/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"adapter-generated-account/generate"
-	"context"
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	echoadapter "github.com/awslabs/aws-lambda-go-api-proxy/echo"
 	"github.com/labstack/echo/v4"
@@ -13,14 +11,16 @@ import (
 	"strconv"
 )
 
-var echoLambda *echoadapter.EchoLambda
+// var echoLambda *echoadapter.EchoLambda
+var echoLambda *echoadapter.EchoLambdaV2
 var e *echo.Echo
 
 func init() {
 	e = echo.New()
 	server := Server{}
 	server.mount(e)
-	echoLambda = echoadapter.New(e)
+	//echoLambda = echoadapter.New(e)
+	echoLambda = echoadapter.NewV2(e)
 }
 
 type Server struct{}
@@ -31,10 +31,10 @@ type GenerateAccountResponse struct {
 }
 
 func (server *Server) mount(e *echo.Echo) {
-	//e.GET("/", func(c echo.Context) error {
-	//	return c.String(http.StatusOK, "Hello, World! Ya")
-	//})
 	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World! Ya")
+	})
+	e.GET("/generate", func(c echo.Context) error {
 
 		/* Initial input parameter */
 		change, err := strconv.Atoi(c.QueryParam("change"))
@@ -61,14 +61,15 @@ func (server *Server) mount(e *echo.Echo) {
 	})
 }
 
-func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	return echoLambda.ProxyWithContext(ctx, req)
-}
+//func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+//	return echoLambda.ProxyWithContext(ctx, req)
+//}
 
 func main() {
 	_, isLambdaMode := os.LookupEnv("LAMBDA_TASK_ROOT")
 	if isLambdaMode {
-		lambda.Start(Handler)
+		//lambda.Start(Handler)
+		lambda.Start(echoLambda.ProxyWithContext)
 	} else {
 		log.Fatal(e.Start(":1234"))
 	}
