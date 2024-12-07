92f4f073ae742a915f10cb8717377dc715b032e1
nata6ify@gmail.com <nata6ify@gmail.com>
2024-12-07 16:15:43 +0700

    [adapter-generate-account] change to echo v2 (2)

--- adapter-generated-account/main.go
+++ adapter-generated-account/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"adapter-generated-account/generate"
+	"context"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	echoadapter "github.com/awslabs/aws-lambda-go-api-proxy/echo"
 	"github.com/labstack/echo/v4"
@@ -11,15 +13,15 @@ import (
 	"strconv"
 )
 
-// var echoLambda *echoadapter.EchoLambda
-var echoLambda *echoadapter.EchoLambdaV2
-var e *echo.Echo
+var (
+	e          *echo.Echo
+	echoLambda *echoadapter.EchoLambdaV2
+)
 
 func init() {
 	e = echo.New()
 	server := Server{}
 	server.mount(e)
-	//echoLambda = echoadapter.New(e)
 	echoLambda = echoadapter.NewV2(e)
 }
 
@@ -32,9 +34,7 @@ type GenerateAccountResponse struct {
 
 func (server *Server) mount(e *echo.Echo) {
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World! Ya")
-	})
-	e.GET("/generate", func(c echo.Context) error {
+		//e.GET("/generate", func(c echo.Context) error {
 
 		/* Initial input parameter */
 		change, err := strconv.Atoi(c.QueryParam("change"))
@@ -51,7 +51,6 @@ func (server *Server) mount(e *echo.Echo) {
 
 		/* Generate keypair */
 		pvk, pub := generate.GetKeypair(account)
-
 		return c.JSON(http.StatusOK, GenerateAccountResponse{
 			Address:    account.Address.String(),
 			PrivateKey: pvk,
@@ -61,15 +60,14 @@ func (server *Server) mount(e *echo.Echo) {
 	})
 }
 
-//func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-//	return echoLambda.ProxyWithContext(ctx, req)
-//}
+func Handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+	return echoLambda.ProxyWithContext(ctx, req)
+}
 
 func main() {
 	_, isLambdaMode := os.LookupEnv("LAMBDA_TASK_ROOT")
 	if isLambdaMode {
-		//lambda.Start(Handler)
-		lambda.Start(echoLambda.ProxyWithContext)
+		lambda.Start(Handler)
 	} else {
 		log.Fatal(e.Start(":1234"))
 	}
